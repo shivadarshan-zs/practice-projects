0385318f14eec14c5157533409e375abea5c04fb
shivadarshan-zs <shivadarshan.m@zopsmart.com>
2026-04-15 15:45:57 +0530

    integrated redis for caching

---
+++ src/main/java/example/question_service/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package example.question_service.config;
+
+import com.example.question_service.model.QuestionWrapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, QuestionWrapper> userRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, QuestionWrapper> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(RedisSerializer.json());
+        return template;
+    }
+}
